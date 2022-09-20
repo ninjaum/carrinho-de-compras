1f0df9cfa81bd5e5a77316f022d0573c8c11b4e9
ninjaum <109167658+ninjaum@users.noreply.github.com>
2022-09-19 21:37:00 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+const controleDeUsuarios = require('./ctrl/controle-de-usuarios')
+const controleDeProdutos = require('./ctrl/controle-de-produtos')
+const port = 3000
 
+const app = express()
+app.use(cors())
+app.use(express.json())
+controleDeUsuarios(app)
+controleDeProdutos(app)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
